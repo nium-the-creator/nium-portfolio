ef8775a412ab0e216ffb99da61e7ec1f29bc2585
Adonis Dominium <peteriyevhobu6@gmail.com>
2026-05-19 17:58:27 +0100

    added social links

--- lib/contact-links.ts
+++ lib/contact-links.ts
@@ -1,7 +1,7 @@
 export const SOCIAL_LINKS = [
-  { label: "Email", href: "mailto:peteriveyhou6@gmail.com" },
-  { label: "Instagram", href: "https://instagram.com" },
-  { label: "Pintrest", href: "https://pinterest.com" },
-  { label: "X (Twitter)", href: "https://x.com" },
-  { label: "LINKEDIN", href: "https://linkedin.com" },
+  { label: "Email", href: "mailto:peteriyevhobu6@gmail.com" },
+  { label: "Instagram", href: "https://www.instagram.com/heis_nium" },
+  { label: "Pintrest", href: "https://pin.it/47XYDYBDN" },
+  { label: "X (Twitter)", href: "https://x.com/nium_aftw" },
+  { label: "LINKEDIN", href: "https://linkedin.com/in/iyevhobu-peter" },
 ] as const;
